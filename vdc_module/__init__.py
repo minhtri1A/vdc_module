3d03f71d0b9b16ff7eef7d7a078eb500eab33afa
MinhTri-NTL <minhtri-ntl@192.168.31.216.non-exists.ptr.local>
2025-05-06 07:47:29 +0700

    edit import module

--- vdc_module/__init__.py
+++
@@ -1,2 +0,0 @@
-from .voice.text_to_speech import generate_text_to_speech
-from .voice.speech_to_text import generate_text
---
+++ vdc_module/voice/__init__.py
@@ -0,0 +1,4 @@
+from .text_to_speech import generate_text_to_speech
+from .speech_to_text import generate_speech_to_text
+
+__all__ = ["generate_text_to_speech", "generate_speech_to_text"]
